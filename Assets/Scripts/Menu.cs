895a80b3a3976a028e9733ffd96db18252ecadd1
kimehunsu <101962235+kimehunsu@users.noreply.github.com>
2022-03-26 21:33:11 +0900

    menu
    sceneLoad

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+
+
+ public void OnClick()
+    {
+        SceneManager.LoadScene("TestScenes");
+    }
+ 
+    public void OnClickQuitGame()
+    {
+       #if UNNITY_EDITOR
+       UnityEditor.EditorAppliiiication.isPlaying =false;
+# else
+        Application.Quit();
+#endif
+    }
+}
