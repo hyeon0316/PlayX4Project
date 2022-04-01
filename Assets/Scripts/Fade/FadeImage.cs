f6d91f5f61fa3cb5855e92ed338ae8176fbcc0cf
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2022-04-01 13:25:09 +0900

    페이드 기능 추가 및 NPC 애니메이션 추가

---
+++ Assets/Scripts/Fade/FadeImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeImage : MonoBehaviour, IFade
+{
+    private Image _fadeImage;
+
+    private void Awake()
+    {
+        _fadeImage = GetComponent<Image>();
+    }
+
+    public void FadeIn()
+    {
+        _fadeImage.enabled = true;
+        StartCoroutine(FadeInCo());
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutCo());
+        _fadeImage.enabled = false;
+    }
+    
+    IEnumerator FadeInCo()
+    {
+        float fadeCount = 0f;
+        while (fadeCount <= 1.0f)
+        {
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            _fadeImage.color = new Color(0, 0, 0, fadeCount);
+        }
+    }
+
+    IEnumerator FadeOutCo()
+    {
+        float fadeCount = 1f;
+        while (fadeCount > 0)
+        {
+            fadeCount -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            _fadeImage.color = new Color(0, 0, 0, fadeCount);
+        }
+    }
+}
