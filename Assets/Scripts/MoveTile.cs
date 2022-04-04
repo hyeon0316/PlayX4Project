579611c15390abebe8aac452329ecaabe52d7c6b
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2022-04-05 06:37:07 +0900

    움직이는 material추가 및 post-processing 효과 조정

---
+++ Assets/Scripts/MoveTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTile : MonoBehaviour
+{
+    private Material _mat;
+    private float _moveOffset;
+
+    public bool CanMoveTile { get; set; }//캐릭터가 맵에 들어왔을때만 타일을 움직이게 함
+    private void Awake()
+    {
+        _mat = GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        if (CanMoveTile)
+        {
+            _moveOffset += 0.003f;
+            _mat.SetTextureOffset("_MainTex", new Vector2(_moveOffset * 0.1f, 0));
+        }
+    }
+}
