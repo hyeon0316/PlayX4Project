dc0aedba81be45b7dcefed67ed8e68cb5d932ece
ujuj3602 <ujuj3602@naver.com>
2022-04-10 21:23:57 +0900

    플레이어 구르기, 벽점프 구현
    벽점프 버그 살짝 있음

--- Assets/Scripts/Life/Player/Player.cs
+++ Assets/Scripts/Life/Player/Player.cs
@@ -23,6 +23,9 @@ public class Player : Life,I_hp
     /// </summary>
     public PlayerstateEnum Playerstate;
 
+    /// <summary>
+    /// 플레이어 총알 메모리풀
+    /// </summary>
     public Transform BulletParent;
     public GameObject[] BulletPool;
 
@@ -67,8 +70,10 @@ public class Player : Life,I_hp
     /// 플레이어가 대화하고 있는지 확인하느변수
     /// </summary>
     public bool IsStop = false;
-
-
+    /// <summary>
+    /// 벽타고 있는중인지 확인
+    /// </summary>
+    private bool _isWallslide = false;
 
     public void Awake()
     {
@@ -170,10 +175,12 @@ public class Player : Life,I_hp
             float h = Input.GetAxisRaw("Horizontal");//x축 으로 이동할때 사용할 변수, 받을 입력값 : a,d
             float v = Input.GetAxisRaw("Vertical");//z 축으로 이동할때 사용할 변수, 받을 입력값 : w,s
             //플레이어가 가는 방향으로 보도록 sprite 돌려주기
-            if (Input.GetButton("Horizontal"))
+            if (Input.GetButton("Horizontal") && !_isWallslide)
             {
             this.transform.GetChild(0).localScale = new Vector3(Input.GetAxisRaw("Horizontal") == -1 ? -2.5f : 2.5f, 2.5f, 1);
             }
+           
+
             //방향키가 눌렸을때에는 달리는 상태로 아니면 idel 상태로 둔다
             if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
             {
@@ -200,22 +207,41 @@ public class Player : Life,I_hp
     private void PlayerJump()
     {
         //대화중이 아닐때만 점프
-        if (!IsStop) { 
+        if (!IsStop) {
 
-            if (Input.GetKey(KeyCode.C))
+            //플레이어가 공중에 있는지 확인하여 공중에 떠있지 않을때만 점프를 할 수 있도록 설정
+           
+            if (Input.GetKeyDown(KeyCode.C))
             {
-                //플레이어가 공중에 있는지 확인하여 공중에 떠있지 않을때만 점프를 할 수 있도록 설정
                 if (!_isFry)
                 {
                     //플레이어가 y 축으로 올라갈 수 있도록 velocity 를 재설정
                     gameObject.GetComponent<Rigidbody>().velocity =
                         new Vector3(_rigid.velocity.x, 1 * 8.5f, _rigid.velocity.z);
                     //점프 애니메이션
-                    _playerAnim.SetBool("IsJump",true);
+                    _playerAnim.SetBool("IsJump", true);
                 }
+                else
+                {
+                    //벽에 슬라이드중
+                    if (_isWallslide)
+                    {
+                        gameObject.GetComponent<Rigidbody>().velocity =
+                      new Vector3(this.transform.GetChild(0).localScale.x < 0 ? -5f : 5f , 1 * 8.5f, _rigid.velocity.z);
+                        //점프 애니메이션
+                        _isWallslide = false;
+                        _playerAnim.SetBool("IsJump", true);
+                        _playerAnim.SetTrigger("WallSlideout");
+                        Physics.gravity = Vector3.down * 25f;
+                    } 
+                }
+
             }
+           
         }
     }
+
+   
 
     /// <summary>
     /// 플레이어가 하늘을 날고 있는지 확인하는 하여 _isFry 변수를 변경하는 함수
@@ -225,7 +251,10 @@ public class Player : Life,I_hp
         RaycastHit hit;
         Ray ray = new Ray(transform.position, Vector3.down);//플레이어 기준으로 아래 방향으로 ray 생성
         //아래 방향을로 ray 를 발사하여 Floor layer 만 충돌하도록 설정
-        if(Physics.Raycast(ray, out hit, LayerMask.GetMask("Floor"))){
+
+        LayerMask layerMask = LayerMask.GetMask("Floor", "Wall", "InterationObj");
+
+        if(Physics.Raycast(ray, out hit, layerMask)){
             //바닥과 플레이어 사이의 거리
             float Distance = hit.distance;
             //바닥과의 거리가 1f 이상 떨어지고 플레이어의 힘이 위쪽을 향하고 있다면
@@ -243,8 +272,14 @@ public class Player : Life,I_hp
             if(_isFry && Distance < 0.7f)
             {
                 _playerAnim.SetBool("IsFall", false);
+                _playerAnim.SetBool("IsJump", false);
                 Debug.Log("Floor충돌");
                     ChangeFry(false);
+                if (_isWallslide) { 
+                    _isWallslide = false;
+                    _playerAnim.SetTrigger("WallSlideout");
+                    Physics.gravity = Vector3.down * 25f;
+                }
             }
         }
     }
@@ -279,7 +314,7 @@ public class Player : Life,I_hp
             }
               
             //공격을 하지 않고 애니메이션이 2f 정도 지났을때 일반 idel 로 초기화
-            if ( _playerAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 2f )
+            if ( _playerAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 3f )
             {
                 CountTimeList[1] = 1f;
                 _playerAnim.SetTrigger("AgainAttackreset");
@@ -302,7 +337,7 @@ public class Player : Life,I_hp
 
     public void Skill()
     {
-        if(Playerstate != PlayerstateEnum.Skill)
+       
         switch (Input.inputString)
         {
             case "a":
@@ -327,6 +362,12 @@ public class Player : Life,I_hp
 
                 }
                 break;
+            case "f":
+            case "F":
+                    if(Playerstate != PlayerstateEnum.Attack)
+                    Roll();
+                break;
+
         }
     }
 
@@ -374,6 +415,8 @@ public class Player : Life,I_hp
             _playerEffectAnim.SetTrigger("Skill2");
             yield return new WaitForSecondsRealtime(0.31f);
         }
+        yield return new WaitForSecondsRealtime(0.09f);
+        Playerstate = PlayerstateEnum.Idle;//스킬이 끝나는 타이밍
     }
 
 
@@ -392,7 +435,7 @@ public class Player : Life,I_hp
         }
         BulletPool[index].GetComponent<Bullet>().Power = Power;
         BulletPool[index].GetComponent<Bullet>().Speed = 5;
-        Playerstate = PlayerstateEnum.Idle;//스킬이 끝나는 타이밍
+        
     }
 
    
@@ -409,6 +452,27 @@ public class Player : Life,I_hp
         }
         return index;
     }
+
+    public void Roll()
+    {
+        _playerAnim.SetTrigger("Roll");
+        StartCoroutine(RollCor());
+    }
+
+    IEnumerator RollCor()
+    {
+        Vector3 dic = this.transform.GetChild(0).localScale.x < 0 ? Vector3.left : Vector3.right;
+        float Distance = 10f; 
+        Playerstate = PlayerstateEnum.Skill;
+        CountTimeList[0] = 3f;
+       for(int i = 0; i < 13; ++i) { 
+            this.transform.Translate(dic* Distance * Time.deltaTime);
+            Debug.Log("Roll 코루틴 확인");
+            yield return new WaitForFixedUpdate();
+        }
+        Playerstate = PlayerstateEnum.Idle;
+        CountTimeList[0] = 0.01f;
+    } 
 
     /// <summary>
     /// 플레이어 UI 업데이트 할 때 사용할 예정이 함수(데이터 전달 및 갱신)
@@ -430,6 +494,28 @@ public class Player : Life,I_hp
         _isFry = p_Fry;
     }
 
-   
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("순서0");
+        if(Mathf.Abs(Input.GetAxis("Vertical")) < 0.1f) { 
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Wall")){
+                Debug.Log("순서1");
+                    
+                    if (_isFry && !_isWallslide)
+                    {
+
+                    Physics.gravity = Vector3.down * 5f;
+                    
+                        this.transform.GetChild(0).localScale = new Vector3(this.transform.GetChild(0).localScale.x * -1,
+                        this.transform.GetChild(0).localScale.y,
+                        this.transform.GetChild(0).localScale.z);
+                         _playerAnim.SetTrigger("WallSlide");
+                        _isWallslide = true;
+                    }
+            
+            }
+        }
+
+    }
 }
