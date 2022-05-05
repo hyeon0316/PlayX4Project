50a6585ba754f027bd173ccc41da2936be25856b
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2022-05-06 02:38:55 +0900

    인트로씬 구현

--- Assets/Scripts/Menu/Lobby.cs
+++ Assets/Scripts/Menu/Lobby.cs
@@ -1,14 +1,82 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class Lobby : MonoBehaviour
 {
+    public Queue<string> Sentences = new Queue<string>();
+    
     public GameObject ManualWindow;
     public GameObject ManualPages;
+    public GameObject EventWindow;
+    private Text _eventText;
+    public string[] IntroSentences;
+    private string _currentSentence;
+
+    private float _textDelay = 0.1f;
+    private void Awake()
+    {
+        _eventText = EventWindow.GetComponentInChildren<Text>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(Sentences.Count);
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene("Town");
+        //todo: 검은이미지 배경에 텍스트 출력 이후 씬 이동
+        EventWindow.SetActive(true);
+        OnDialogue(IntroSentences);
+    }
+    
+    public void OnDialogue(string[] lines)
+    {
+        Sentences.Clear();
+        foreach (string line in lines)
+        {
+            Sentences.Enqueue(line);
+        }
+        NextSentence();
+    }
+
+    private void NextSentence()
+    {
+        if (Sentences.Count != 0 && !Sentences.Peek().Equals("Next") && !Sentences.Peek().Equals("Start"))
+        {
+            PrintSentences();
+        }
+        else if (Sentences.Peek().Equals("Next"))
+        {
+            Debug.Log("줄 바꿈");
+            Sentences.Dequeue();
+            _eventText.text += "\n" + "\n";
+            PrintSentences();
+        }
+        else if (Sentences.Peek().Equals("Start"))
+        {
+            SceneManager.LoadScene("Town");
+        }
+    }
+    private void PrintSentences()
+    {
+        _currentSentence = Sentences.Dequeue();
+        StartCoroutine(IntroCo(_currentSentence));
+    }
+
+    private IEnumerator IntroCo(string line)
+    {
+        yield return new WaitForSeconds(1f);
+        foreach (char letter in line.ToCharArray())
+        {
+            _eventText.text += letter;
+            yield return new WaitForSeconds(_textDelay);
+        }
+        NextSentence();
     }
 
     public void ShowManual()
