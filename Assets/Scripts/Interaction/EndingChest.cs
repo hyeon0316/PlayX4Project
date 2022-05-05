df85e525cdc6afe710b50804b94de8444e60be37
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2022-05-06 05:20:01 +0900

    엔딩씬 구현중

---
+++ Assets/Scripts/Interaction/EndingChest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingChest : Interaction
+{
+   private FadeImage _fade;
+   protected override void Awake()
+   {
+      base.Awake();
+      this.GetComponent<BoxCollider>().enabled = false;
+      _fade = FindObjectOfType<FadeImage>();
+   }
+
+   private void Update()
+   {
+      StartInteract();
+   }
+
+   public override void StartInteract()
+   {
+      if (CanInteract)
+      {
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+            FindObjectOfType<Inventory>().DeleteMaterial();
+            _fade.FadeIn();
+            this.GetComponent<Animator>().SetTrigger("Open");
+            CanInteract = false;
+         }
+      }
+
+      if (_fade.IsFade)
+      {
+         //todo: 엔딩씬 이동
+      }
+      
+   }
+}
