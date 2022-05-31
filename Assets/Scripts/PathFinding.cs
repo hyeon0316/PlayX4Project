d0223c22e0da0c7cdc4b84d1ce0d824512ce0a05
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2022-05-31 14:27:26 +0900

    .

---
+++ Assets/Scripts/PathFinding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFinding : MonoBehaviour
+{
+    private NavMeshAgent _navItem;
+
+    private void Awake()
+    {
+        _navItem = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        _navItem.SetDestination(GameObject.Find("NavCube").transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_navItem.remainingDistance <=_navItem.stoppingDistance)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject NavItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(NavItem,FindObjectOfType<Player>().transform.position,FindObjectOfType<Player>().transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
